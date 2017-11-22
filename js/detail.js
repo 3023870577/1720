8805064afb466ae045af6385714c54006f9489da
chenqi <3023870577@qq.com>
2017-11-22 18:17:40 +0800

    星期三

--- js/detail.js
+++ js/detail.js
@@ -25,7 +25,7 @@ requirejs(["jquery","cookie"],function($,cookie){
             // 顶部悬浮
             $(window).scroll(function () {
                 var $top = $(document).scrollTop();
-                console.log($top);
+                // console.log($top);
                 var $nav = $(".position-content");
                 if ($top >= 200&&$top<=800) {
                     $nav.css({ "position": "fixed", "top": "0", "width": "100%" });
@@ -56,6 +56,129 @@ requirejs(["jquery","cookie"],function($,cookie){
                 $(".logined").css("display", "none");
             }
         });    
+    })
+    // 页面本身的效果
+    $(function(){
+        $(window).scroll(function(){
+            var $top = $(document).scrollTop();
+            var $nav = $(".detail-nav");
+            if($top > 800){
+                $nav.css({ "position": "fixed", "top": "-50px","box-shadow": "0 1px 1px #eee" });
+                $(".nav-buy-btn").css("display","block");
+            }else{
+                $nav.css({ "position": "relative", "top": "null" })
+                $(".nav-buy-btn").css("display", "none");
+            }
+        })
+    })
+    // 产品相册
+    $(function(){
+        var $prevBtn = $(".prev");
+        var $nextBtn = $(".next");
+        var $li = $(".thumb-list li"); 
+        // 下按钮
+        var toggle = true;
+        var toggler = true;
+        $nextBtn.click(function(){
+            if(toggle){
+                toggle = false;
+                var $top = parseInt($(".thumb-list").css("margin-top"));
+                if(Math.abs($top) >= 152){
+                    $(".thumb-list").css("margin-top",$top);
+                    toggle = true;
+                }else{
+                    $(".thumb-list").animate({"margin-top":$top - 76 + "px"},500,function(){
+                        toggle = true;
+                    });
+                }
+            }  
+        })
+        // 上按钮
+        $prevBtn.click(function(){
+            if(toggler){
+                toggler = false;
+                var $top = parseInt($(".thumb-list").css("margin-top"));
+                if (Math.abs($top) <= 0) {
+                    $(".thumb-list").css("margin-top", $top);
+                    toggler = true;
+                } else {
+                    $(".thumb-list").animate({ "margin-top": $top + 76 + "px" }, 500,function(){
+                        toggler = true; 
+                    });
+                }
+            }
+            
+        })
+        var $pro = $(".pro-album-preview img");
+        var $proImg = $(".pro-album-big img");
+        var $mask = $(".mask");
+        var $bigMask = $(".big-mask");
+        // 小图
+        $li.mouseenter(function(){
+            var $index = $(this).index();
+            $(this).addClass("act").siblings().removeClass("act");
+            $pro.eq($index).addClass("active").siblings().removeClass("active");
+            $proImg.eq($index).addClass("active").siblings().removeClass("active");
+        })
+        //中图
+        $bigMask.mouseover(function(e){
+            $(".pro-album-big").css("display","block");
+            $mask.css("display", "block");
+            var e = e || event;
+        })
+        //放大镜效果
+        $bigMask.mousemove(function(e){
+            var e = e || event;
+            var left = e.pageX - $(this).offset().left - $mask.outerWidth()/2;
+            var top = e.pageY - $(this).offset().top - $mask.outerHeight()/2;  
+            var maxL = $(".pro-album-preview").outerWidth() - $mask.outerWidth();
+            var maxT = $(".pro-album-preview").outerHeight() - $mask.outerHeight()
+            left = Math.min(maxL , Math.max(0,left));
+            top = Math.min(maxT , Math.max(0,top));
+            $mask.css({"left":left,"top":top});
+            var percentX = left/($bigMask.outerWidth() - $mask.outerWidth());
+            var percentY = top/($bigMask.outerHeight() - $mask.outerHeight());
+            var bigLeft = percentX * ($proImg.outerWidth() - $(".pro-album-big").outerWidth());
+            var bigTop = percentY * ($proImg.outerHeight() - $(".pro-album-big").outerHeight());
+            $proImg.css({"left":-bigLeft,"top":-bigTop});
+        })
+
+        $bigMask.mouseout(function(){
+            $(".pro-album-big").css("display", "none");
+            $mask.css("display", "none");
+        })
+    })
+    $(function(){
+        var $increaseBtn = $(".btn-increase");
+        var $decreaseBtn = $(".btn-decrease");
+        var $buyNum = $(".buy-num");
+        var $val = $buyNum.val();
+        var $pri = $(".action-price").html();
+        $increaseBtn.click(function(){
+            $buyNum.attr({"value":++$val});
+            $(".action-price").html($val*$pri+".00");
+        })
+        $decreaseBtn.click(function () {
+            if($val ==1){
+                $buyNum.attr({ "value": $val });
+                $(".action-price").html($val * $pri + ".00");
+            }else{
+                $buyNum.attr({ "value": --$val });
+                $(".action-price").html($val * $pri + ".00");
+            }
+            
+        })
+    })
+    // 锚点导航
+    $(function(){
+        $(".list-item").hover(function(){
+            $(this).find($("a")).css("color","#d92a4b");
+        },function(){
+            $(this).find($("a")).css("color", "#333333");
+        })
+        $(".list-item").click(function(){
+            $(this).addClass("cur").siblings().removeClass("cur");
+        })
     })
     //公共尾部  
     $(function () {
